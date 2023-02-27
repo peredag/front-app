0cee610a3081831155390fa00e5e943a31c4ef7a
greta <greta.pereda@gmail.com>
2023-02-27 15:36:42 -0600

    index padre de hero y de register

---
+++ src/components/index/Index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero from '../hero/Hero'
+import Register from '../register/Register'
+
+export default function Index() {
+  return (
+    <div>
+      <Hero />
+      <Register />
+    </div>
+  )
+}
